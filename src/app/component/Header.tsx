2b72d4c7350dd728903c468ea7c12f031f018564
RaviGupta965 <clicktoravi1234@gmail.com>
2025-08-31 01:01:13 +0530

    responsiveness-added

--- src/app/component/Header.tsx
+++ src/app/component/Header.tsx
@@ -3,14 +3,21 @@ import React from 'react'
 
 function Header() {
   return (
-    <div className=' w-[100%] px-6 border-b-[rgba(234,236,240,1)] border-b-2'>
-      <div className='py-5 flex gap-1 flex-row justify-end items-center'>
-        <div className=''>
-          <Image height={16} width={16} src='/Settings.png' alt='actions' className='' />
+    <div className="w-full px-6 border-b-[rgba(234,236,240,1)] border-b-2">
+      <div className="py-5 flex flex-row justify-between items-center lg:justify-end gap-1">
+        {/* Hamburger: visible on mobile only */}
+        <div className="lg:hidden">
+          Hamburger
         </div>
-        <div className="relative w-[40px] h-[40px]">
-          <Image height={36} width={40} src="/Avatar_Border.png" alt="Background" />
-          <Image height={40} width={40} src="/Avatar.png" className="rounded-full absolute top-0 left-0" alt="Overlay" />
+
+        {/* Settings icon & avatar: always visible */}
+        <div className="flex flex-row items-center gap-2">
+          <div>
+            <Image height={16} width={16} src="/Settings.png" alt="actions" />
+          </div>
+          <div className="w-[40px] h-[40px]">
+            <Image height={40} width={40} src="/Avatar.png" className="rounded-full" alt="Overlay" />
+          </div>
         </div>
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,25 @@
+'use client'
+import { useState } from "react";
 import Sidebar from "./component/Sidebar";
 import Header from "./component/Header";
 import Accounts from "./component/Accounts";
 import Controller from "./component/Controller";
 export default function Home() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => setSidebarOpen(!sidebarOpen);
   return (
     <div className="flex flex-row">
       <div className="flex flex-row">
-        <Sidebar />
-        <div className="flex flex-col w-[76vw]">
+        <div className={`
+    block h-full bg-white w-[max(22vw,240px)]
+    transition-left duration-300
+    lg:left-0
+    ${sidebarOpen ? "left-0" : "-left-full"}
+  `}>
+          <Sidebar />
+        </div>
+        <div className="flex flex-col lg:w-[76vw] w-[100vw]">
           <Header />
           <Accounts />
           <Controller />
